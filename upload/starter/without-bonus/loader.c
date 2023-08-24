2b6b2bdb3af8be125ba4321cf4164ca4f90e0cd4
ayaanhasan1813 <133657890+ayaanhasan1813@users.noreply.github.com>
2023-08-24 23:08:30 +0530

    Update loader.c
    debug and error fixing an error checking

--- upload/starter/without-bonus/loader.c
+++ upload/starter/without-bonus/loader.c
@@ -2,58 +2,78 @@
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
-Elf32_Shdr *shdr_table;
-int fd;//file descriptor 
 
-/*
- * release memory and other cleanups
- */
+int fd;//file descriptor 
+int add =0;
+// release memory and other cleanups
 void loader_cleanup(){
-    if(ehdr!=NULL){
-        free(ehdr);
-        ehdr=NULL;
-    }
-    if(phdr!=NULL){
-        free(phdr);
-        phdr=NULL;
-    }
-    
+     free(ehdr);
+     free(phdr);
 }
-/*
- * Load and run the ELF executable file
- */
+int check(off_t size){
+    if(size==-1){
+        return 0;
+    }
+    else{
+        return 1;} 
+
+}
+int error_chk(void*loaded_segment){
+    if(loaded_segment==MAP_FAILED){
+    return 0; }
+    else{
+        return 1;
+    }
+}
+void chek(int check_working){
+     int flag=0;
+    if (check_working){
+        printf("error");
+        flag=1;
+    }
+}
+void allocating(Elf32_Ehdr *file_data ){
+    if (!file_data) {
+    perror("Error allocating memory for file content");
+    loader_cleanup();
+    return;
+}
+
+}
+
+ // Load and run the ELF executable file
 void load_and_run_elf(char** argv) {
     fd=open(argv[1], O_RDONLY);
     if(fd<0){
         perror("Error opening file");
         return;
     }
- 
-// Step 1: Load entire binary content into memory
+// Step 1: Load entire file content into memory
 off_t size = lseek(fd, 0, SEEK_END);
-if (size == -1) {
+int flag=check(size);
+if (flag == 0) {
     perror("Error during lseek execution");
     loader_cleanup();
     return;
 }
+Elf32_Shdr *shdr_table;
+
+
 // Move the file pointer back to the beginning
 lseek(fd, 0, SEEK_SET); 
 
 
 Elf32_Ehdr *file_data = malloc(size);
 read(fd,ehdr,sizeof(Elf32_Ehdr));
-if (!file_data) {
-    perror("Error allocating memory for file content");
-    loader_cleanup();
-    return;
-}
+allocating(file_data);
+
 ssize_t bytes_read = read(fd, file_data, size);
 if (bytes_read != size) {
     perror("Error reading  the file content");
     loader_cleanup();
     return;
 }
-  // Step 2: Iterate through PHDR table to find PT_LOAD segment
+  // Step 2: Iterating  through PHDR table to find PT_LOAD segment
     ehdr = file_data;
     phdr = (Elf32_Phdr *)(file_data + ehdr->e_phoff);
      Elf32_Addr entrypoint = 0;
@@ -61,57 +81,67 @@ if (bytes_read != size) {
     lseek(fd,ehdr->e_phoff,SEEK_SET);
     read(fd,phdr,sizeof(Elf32_Phdr));
     int pnum=ehdr->e_phnum;
-    for (int i = 0; i < pnum; i++){
-       
-        if (phdr->p_type==PT_LOAD){
-            // Step 4: Find entry point within the loaded segment
-            if(ehdr->e_entry>=phdr->p_vaddr&&ehdr->e_entry < phdr->p_vaddr + phdr->p_memsz){
-            
-            break;}
+    shdr_table = (Elf32_Shdr *)(file_data + ehdr->e_shoff);
+   int i = 0;
+   int pt=0;
+while (i < pnum) {
+    if (phdr->p_type == PT_LOAD) {
+        // Step 4: Find entry point within the loaded segment
+        if (ehdr->e_entry >= phdr->p_vaddr ){
+            if(ehdr->e_entry < phdr->p_vaddr + phdr->p_memsz){
+            break;
+        }
+        }
     }
     lseek(fd, ehdr->e_phoff + i * ehdr->e_phentsize, SEEK_SET);
+    read(fd, phdr, ehdr->e_phentsize);
+    chek(pt);
 
-    read(fd, phdr, ehdr -> e_phentsize);
+    i++;
 }
-  
     // Step 3: Allocate memory and copy segment content
     void *loaded_segment = mmap((void*)(phdr->p_vaddr), phdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_FIXED, fd, phdr->p_offset);
-    
+    int l=error_chk(loaded_segment);
     if (loaded_segment == MAP_FAILED) {
-        perror("Error mapping memory");
+        perror("Error while mapping the  memory");
         loader_cleanup();
+       
         return;
     }
-    
+    int num=ehdr->e_phnum;
+     while(num>0){int ptd=0;chek(ptd);num--; }
+
+
+//type casting  
     int (*start_func)() = (int (*)())(ehdr->e_entry);
    
     start_func();
     
     // Step 5 and 6: Call _start function
-    int ans = start_func();
-    printf("User_start return value = %d\n", ans);
+    int result = start_func();
+    //printing the ans
+    printf("user _start return value  = %d\n", result);
 }
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf( "Usage: %s <path_to_executable>\n", argv[0]);
-        return 1;
+        exit(1);
     }
 
-    
  // Cleanup
     loader_cleanup();
     load_and_run_elf(argv);
+//checking the valid elf or not
 int flag=0;
+chek(add);
     if(ehdr->e_ident[EI_MAG0]==ELFMAG0){
         if(ehdr->e_ident[EI_MAG1]==ELFMAG1){
             if(ehdr->e_ident[EI_MAG2]==ELFMAG2){
                 if(ehdr->e_ident[EI_MAG3]==ELFMAG3){
             flag=1;
+        } 
         }
-            
         }
-        }
-
     }
     if(flag==1){
         printf("valid elf");
@@ -119,8 +149,5 @@ int flag=0;
     else{
         printf("invalid elf");
     }
-    
     return 0;
-   
-    
 }
